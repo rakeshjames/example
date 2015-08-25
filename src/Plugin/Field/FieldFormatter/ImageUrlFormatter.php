56dbbde430fa259f073ee5ba3c5c2f2ba4f87cfe
rakesh-james <rakesh.james@valuebound.com>
2015-08-25 12:14:47 +0530

    adding image url formatter

---
+++ src/Plugin/Field/FieldFormatter/ImageUrlFormatter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\example\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'youtube_link' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "image_url",
+ *   label = @Translation("Image URL"),
+ *   field_types = {
+ *     "image"
+ *   }
+ * )
+ */
+class ImageUrlFormatter extends FormatterBase {
+    /**
+     * {@inheritdoc}
+     */
+    public function viewElements(FieldItemListInterface $items) {
+        $elements = array();
+
+        foreach ($items as $delta => $item) {
+
+            $url = $item->uri;
+            $elements[$delta] = array(
+                '#theme' => 'image_url_formatter',
+                '#url' => $url,
+            );
+        }
+
+        return $elements;
+    }
+}
